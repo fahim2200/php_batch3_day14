2c673c2e8947d3b867f5c8a01fde8f29449b95ef
fahim2200 <fahimrahman223@gmail.com>
2022-02-03 11:38:43 +0600

    string

---
+++ app/classes/Student.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Student
+{
+    public function getAllStudent()
+    {
+        return[
+            0=> [
+                'name'      => 'Shahabuddin',
+                'mobile'    => ' 124564454',
+                'email'     => 'sahabuddin@gmail.com',
+                'address'   => 'lalbagh'
+            ],
+            1=> [
+                'name'      => 'rafa',
+                'mobile'    => ' 134564454',
+                'email'     => 'rafa@gmail.com',
+                'address'   => 'rampura'
+            ],
+            2=> [
+                'name'      => 'Karim',
+                'mobile'    => '1245564454',
+                'email'     => 'KARIM@gmail.com',
+                'address'   => 'narayanganj'
+            ],
+            3=> [
+                'name'      => 'RAHIM',
+                'mobile'    => '1244454',
+                'email'     => 'rahim@gmail.com',
+                'address'   => 'puran dhaka'
+            ],
+            4=> [
+                'name'      => 'tonmoy',
+                'mobile'    => '124564454',
+                'email'     => 'tonmoy@gmail.com',
+                'address'   => 'jigatola'
+            ],
+        ];
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use App\classes\StringWordCount;
+
+$stringWordCount = new stringWordCount($_POST);
+    $stringWordCount->index();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
